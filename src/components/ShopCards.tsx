7e48852e01f9746b34b75371bdbd9ab38aa9f123
unreact-hatano <mizutama.hahaha@gmail.com>
2023-12-11 15:14:01 +0900

    useShopsをcallする位置を修正
    複数のコンポーネントがレスポンスを利用できるようになった
    paginationのバグ修正

--- src/components/ShopCards.tsx
+++ src/components/ShopCards.tsx
@@ -2,30 +2,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-import { useRecoilValue } from 'recoil';
-
 import CommonImage from '@/components/common/CommonImage';
-import Pagination from '@/components/common/Pagination';
-import { useShops } from '@/hooks/api/useShops';
-import { locationState } from '@/hooks/atom/location';
-import { rangeState } from '@/hooks/atom/range';
-import { startState } from '@/hooks/atom/start';
 import { Shop } from '@/lib/Shop';
 
 type Props = {
+  shops: Shop[];
   isLoading: boolean;
 };
 
 const ShopCards = (props: Props): JSX.Element => {
-  const { isLoading } = props;
-  const location = useRecoilValue(locationState);
-  const range = useRecoilValue(rangeState);
-  const start = useRecoilValue(startState);
-
-  const { data, isSWRLoading, isError } = useShops({ ...location, range, start });
-
-  if (isError) return <div>Error fetching data</div>;
-  if (isSWRLoading) return <div>Loading...</div>;
+  const { shops, isLoading } = props;
 
   if (isLoading)
     return (
@@ -36,31 +22,25 @@ const ShopCards = (props: Props): JSX.Element => {
 
   return (
     <>
-      {data ? (
-        <>
-          <div className="mt-5 grid max-w-7xl grid-cols-3 gap-5">
-            {data.shops.map((shop: Shop) => (
-              <div key={shop.id} className="card card-compact w-96 shadow-xl">
-                <figure className="relative h-32 w-full">
-                  <CommonImage
-                    src={shop.photo.pc.l}
-                    alt="店舗のイメージ"
-                    className="object-cover"
-                    fill
-                  />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title text-base">{shop.name}</h2>
-                  <p className="text-xs">{shop.access}</p>
-                </div>
+      {shops ? (
+        <div className="mt-5 grid max-w-7xl grid-cols-3 gap-5">
+          {shops.map((shop: Shop) => (
+            <div key={shop.id} className="card card-compact w-96 shadow-xl">
+              <figure className="relative h-32 w-full">
+                <CommonImage
+                  src={shop.photo.pc.l}
+                  alt="店舗のイメージ"
+                  className="object-cover"
+                  fill
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-base">{shop.name}</h2>
+                <p className="text-xs">{shop.access}</p>
               </div>
-            ))}
-          </div>
-
-          <div className="flex w-full justify-center">
-            <Pagination totalCount={data.totalCount} resultsStart={data.resultsStart} />
-          </div>
-        </>
+            </div>
+          ))}
+        </div>
       ) : (
         <div className="flex h-96 items-center justify-center">
           <p className="tracking-widest text-gray-400">条件に合う飲食店は見つかりませんでした</p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,18 @@ import { useEffect, useState } from 'react';
 import ShopCards from '@/components/ShopCards';
 import ShopsFilter from '@/components/ShopsFilter';
 import Navbar from '@/components/common/Navbar';
+import Pagination from '@/components/common/Pagination';
+import { useShops } from '@/hooks/api/useShops';
 import { locationState } from '@/hooks/atom/location';
+import { rangeState } from '@/hooks/atom/range';
+import { startState } from '@/hooks/atom/start';
 import { Geolocation } from '@/lib/Geolocation';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 const Home: NextPage = () => {
-  const setLocation = useSetRecoilState(locationState);
+  const range = useRecoilValue(rangeState);
+  const start = useRecoilValue(startState);
+  const [location, setLocation] = useRecoilState(locationState);
   const [isLoading, setIsLoading] = useState(false);
 
   const getCurrentLocation = async () => {
@@ -33,6 +39,11 @@ const Home: NextPage = () => {
     getCurrentLocation();
   }, []);
 
+  const { data, isSWRLoading, isError } = useShops({ ...location, range, start });
+
+  if (isError) return <div>Error fetching data</div>;
+  if (isSWRLoading) return <div>Loading...</div>;
+
   return (
     <>
       <Head>
@@ -49,10 +60,13 @@ const Home: NextPage = () => {
         <Navbar />
         <div className="flex justify-between">
           <div className="mt-8 m-8 w-1/4">
-            <ShopsFilter />
+            <ShopsFilter totalCount={data?.totalCount} />
           </div>
           <div className="mt-8 mr-8 w-3/4">
-            <ShopCards isLoading={isLoading} />
+            <ShopCards shops={data?.shops} isLoading={isLoading} />
+            <div className="flex w-full justify-center">
+              <Pagination totalCount={data?.totalCount} resultsStart={data?.resultsStart} />
+            </div>
           </div>
         </div>
       </main>
