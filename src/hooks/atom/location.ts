44a6341905cff886871be519cfacecf1d606833d
unreact-hatano <mizutama.hahaha@gmail.com>
2023-12-16 12:59:17 +0900

    キャッシュの利用
    ローカルストレージを利用して位置情報をキャッシュし、それを次回の初期値に設定する

--- src/hooks/atom/location.ts
+++ src/hooks/atom/location.ts
@@ -5,7 +5,13 @@ export type Location = {
   lng?: number;
 };
 
+// ローカルストレージから位置情報を取得
+const getLocationFromLocalStorage = (): Location | object => {
+  const location = localStorage.getItem('userLocation');
+  return location ? (JSON.parse(location) as Location) : {};
+};
+
 export const locationState = atom<Location>({
   key: 'locationState',
-  default: {},
+  default: getLocationFromLocalStorage(),
 });
